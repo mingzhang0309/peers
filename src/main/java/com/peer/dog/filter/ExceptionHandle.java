6db5bd721093e8dd7ddbdcb0fa08c78d0bf0a3a5
明涵 <minghan.zm@alibaba-inc.com>
2018-06-24 19:27:46 +0800

    add

--- src/main/java/com/peer/dog/filter/ExceptionHandle.java
+++ src/main/java/com/peer/dog/filter/ExceptionHandle.java
@@ -1,6 +1,8 @@
 package com.peer.dog.filter;
 
 import com.peer.dog.pojo.BaseResponseVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @ControllerAdvice
 public class ExceptionHandle {
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public BaseResponseVO Handle(Exception e){
+        logger.error("异常信息", e);
         return BaseResponseVO.FailureResponse(e.getMessage());
     }
 }
